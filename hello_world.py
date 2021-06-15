1858cdc19dd9dc522e7718620a1c376ad85eb7a2
chencoolhero <coolherochen@gmail.com>
2021-06-15 12:16:17 -0700

    lab 2

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
 print("Hello World")
+name = "Lebron" #string
+age = 35 #int
+salary = 37.44 #float
+plays_basketball = True #boolean
+jersey_number = "23" #string
+
+car_name = "Toyota"
+x = 50
+my_first_name = "Jian Ming"
+
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
